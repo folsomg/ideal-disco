38666da369612d0efa4aca4c2b940ad1817ccca7
folsomg <folsomg@users.noreply.github.com>
2025-11-19 10:28:24 +0200

    Fix typo

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 module.exports = {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
+  // Update: commit 23
     db: {
   // Update: commit 5
         host: process.env.DB_HOST || 'localhost',
